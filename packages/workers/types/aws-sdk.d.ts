56d0ab610171c95fdd17686b93cd682aa7d40e0b
Kai Shoya <shonari77@gmail.com>
2025-12-30 10:34:39 +0900

    chore: prepare packages/workers for git submodule

--- packages/workers/types/aws-sdk.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@aws-sdk/client-s3' {
-  const content: unknown
-  export = content
-}
-
-declare module '@aws-sdk/s3-request-presigner' {
-  const content: unknown
-  export = content
-}
